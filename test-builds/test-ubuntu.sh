bf1596025c13be38707013e8bcff423d23d8e9e8
Faruk D <f.diblen@esciencecenter.nl>
2018-03-01 17:50:01 +0100

    add script to test ubuntu builds

---
+++ test-builds/test-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building SageCal" && \ 
+echo "Branch --> $BRANCH" && \
+cd /travis/workdir && \
+mkdir build && cd build && \
+cmake .. -DENABLE_CUDA=OFF && \
+make -j4 && \
+ls -alsrt ./dist/bin && \
+./dist/bin/sagecal
+
