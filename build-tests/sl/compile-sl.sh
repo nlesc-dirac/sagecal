4b2c475f9f2db94910143cb1456a81307d918426
Faruk D <f.diblen@esciencecenter.nl>
2018-08-28 15:45:47 +0200

    fix casacore build path

--- build-tests/sl/compile-sl.sh
+++ build-tests/sl/compile-sl.sh
@@ -11,13 +11,10 @@ cd /travis/workdir
 # wget -c ftp://ftp.astron.nl/outgoing/Measures/WSRT_Measures.ztar
 # tar zxfv WSRT_Measures.ztar && rm -f WSRT_Measures.ztar
 
-cd /tmp
-git clone --progress --verbose https://github.com/casacore/casacore.git casacore_install
-cd casacore_install
-
 cd /travis/workdir
-mkdir -p casacore/build
-cd casacore/build
+git clone --progress --verbose https://github.com/casacore/casacore.git casacore_src && cd casacore_src
+
+mkdir build && cd build
 cmake3 -DUSE_FFTW3=ON -DCMAKE_INSTALL_PREFIX=/opt/soft/casacore -DDATA_DIR=/opt/soft/casacore/data -DUSE_OPENMP=ON \
     -DUSE_HDF5=ON -DBUILD_PYTHON=ON -DUSE_THREADS=ON ..
 make -j4
