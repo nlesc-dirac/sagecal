52adaee7f0710bd2318f4e0196587fd4eaf3180e
Faruk D <f.diblen@esciencecenter.nl>
2018-08-28 14:54:33 +0200

    add sl compile instructions

---
+++ build-tests/sl/prepare-sl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# add EPEL repository for openblas
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# install dependencies
+yum -y install git pkgconfig cmake cmake-gui gcc-gfortran gcc-c++ flex bison \
+       openblas openblas-devel glib2-devel lapack lapack-devel cfitsio cfitsio-devel \
+       wcslib wcslib-devel ncurses ncurses-devel readline readline-devel\
+       python-devel boost boost-devel fftw fftw-devel hdf5 hdf5-devel\
+       numpy boost-python
