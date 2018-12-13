460fe74021a6a8f933770b0a6a30badc701e827d
Faruk D <f.diblen@esciencecenter.nl>
2018-12-13 11:28:33 +0100

    rename scripts because they wont work on vu das5 system

---
+++ scripts/load_das5_astron_modules_gcc6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+module load cmake/3.8.2
+module load mpich/ge/gcc/64/3.2
+module load gcc/6.3.0
+module load casacore/2.4.1-gcc-6.3.0
+module load wcslib/5.18-gcc-6.3.0
+module load cfitsio/3.430-gcc-6.3.0
+#module load blas/gcc/64/3.7.0
+module load openblas/0.2.20
+
+
+module load cuda91/toolkit/9.1.85
+#module load cuda92/toolkit/9.2.88
