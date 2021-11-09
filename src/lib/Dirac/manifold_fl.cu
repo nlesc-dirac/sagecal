425a256ba334175263c94dbb9e105150def80214
Sarod Yatawatta <sarod@dop373.nfra.nl>
2021-11-09 15:06:43 +0100

    rename headers to have unique names

--- src/lib/Dirac/manifold_fl.cu
+++ src/lib/Dirac/manifold_fl.cu
@@ -21,7 +21,7 @@
 #include <cuComplex.h>
 #include <stdio.h>
 #include <cublas_v2.h>
-#include "GPUtune.h"
+#include "Dirac_GPUtune.h"
 
 /* enable this for checking for kernel failure */
 //#define CUDA_DBG
