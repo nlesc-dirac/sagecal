8644b6c266c64d7e0a641bd4d2f914448b2e5686
Sarod Yatawatta <sarod@dop373.nfra.nl>
2024-04-18 13:19:51 +0200

    add uvwriter

---
+++ src/uvwriter/uvwriter.cpp
@@ -0,0 +1,44 @@
+/*
+ *
+ Copyright (C) 2024 Sarod Yatawatta <sarod@users.sf.net>
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ $Id$
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <casacore/ms/MeasurementSets/MSIter.h>
+#include <casacore/tables/Tables/Table.h>
+#include <casacore/tables/Tables/TableVector.h>
+#include <casacore/tables/Tables/TableRecord.h>
+#include <casacore/tables/Tables/TableColumn.h>
+#include <casacore/tables/Tables/TableIter.h>
+#include <casacore/tables/Tables/ScalarColumn.h>
+#include <casacore/tables/Tables/ArrayColumn.h>
+#include <casacore/casa/Arrays/Array.h>
+#include <casacore/casa/Arrays/Cube.h>
+#include <fstream>
+#include <math.h>
+#include <complex>
+#include <SpiceUsr.h>
+#include "Dirac_radio.h"
+
+
+int 
+main(int argc, char **argv) {
+
+  return 0;
+}
